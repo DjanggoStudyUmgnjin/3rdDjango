cf4357506bf20a9a0dd01cb89cf40c906d0ad018
Yeeun Park <pinkpyy@gmail.com>
2017-02-24 23:30:39 +0900

    enroll course

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.urlresolvers import reverse_lazy
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -30,13 +31,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'courses',
+    'students',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'embed_video',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -98,3 +100,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
